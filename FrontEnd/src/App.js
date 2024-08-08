fbdc08bccbf6431e363af42f2353850f30f44525
shamgirhe <shamgirhe01@gmail.com>
2024-08-08 21:44:31 +0530

    Front End Folder Uploaded.

---
+++ FrontEnd/src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <div>
+      <body>SUNBANK</body>
+    </div>
+  );
+}
+
+export default App;
