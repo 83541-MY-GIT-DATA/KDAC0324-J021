96641612aacc6c8f7116540f0db58dec086221e5
shamgirhe <shamgirhe01@gmail.com>
2024-08-09 19:59:03 +0530

    Front Navbar for the Customer is added

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -2,11 +2,20 @@ import logo from "./logo.svg";
 import "./App.css";
 
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./component/Navbar";
+
+import "react-toastify/dist/ReactToastify.css";
+import { Footer } from "./component/Footer";
 
 function App() {
   return (
     <div>
-      <body>SUNBANK</body>
+      <body>
+        <BrowserRouter>
+          <Navbar></Navbar>
+          <Footer></Footer>
+        </BrowserRouter>
+      </body>
     </div>
   );
 }
