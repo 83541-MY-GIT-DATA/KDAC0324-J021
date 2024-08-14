2a1ad02ea0b4c78539a16aaa14ef92b82fae4250
shamgirhe <shamgirhe01@gmail.com>
2024-08-14 11:57:27 +0530

    Final Commit of Project

---
+++ Project Final Data/FrontEnd/src/Components/contactUsComponent.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./contactUsComponent.css";
+
+function ContactUs() {
+  return (
+    <div className="contact-container">
+      <h2 className="contact-header">Contact Us</h2>
+      <p className="contact-info">Email: sunbank.sunbeam@gmail.com</p>
+      <p className="contact-info">Phone: +91 9359838195</p>
+      <p className="contact-info">Phone: +91 1234567890</p>
+    </div>
+  );
+}
+
+export default ContactUs;
